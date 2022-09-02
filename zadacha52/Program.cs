d78067f3e7a8488ac39a69874c5abc373048c304
jack <tapkin@gmail.com>
2022-09-02 13:38:59 +0300

    dz7

---
+++ zadacha52/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+void FillMatrix(int[,] matrix)                              
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-100, 101);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)                             
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"[{matrix[i, j]}] ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Задайте количество строк массива");     
+int rows = Convert.ToInt32(Console.ReadLine());
+while (rows < 2)
+{
+    Console.WriteLine("Количество строк не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    rows = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Задайте количество столбцов массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+while (cols < 2)
+{
+    Console.WriteLine("Количество столбцов не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    cols = Convert.ToInt32(Console.ReadLine());
+}
+int[,] numbers = new int[rows, cols];
+FillMatrix(numbers);
+PrintMatrix(numbers);
+
+Console.Write("Среднее арифметическое каждого столбца ");
+int[] average = new int[cols];
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        average[i] += numbers[j, i];
+    }
+    average[i] /= numbers.GetLength(0);
+    Console.Write($"({average[i]}) ");
+}
